b261fee8c79b726cbf3c22631a6601d305e96bb7
OscarAE <o_bolivar@hotmail.com>
2025-11-11 15:27:15 -0500

    app.py posicion titulo

--- app.py
+++ app.py
@@ -816,8 +816,8 @@ def funcionario_print_day():
     # Logo centrado
     try:
         logo_width = 400
-        logo_height = 5
-        c.drawImage("static/titulo.png", (w-logo_width)/2, h-150, width=logo_width, height=logo_height)
+        logo_height = 60
+        c.drawImage("static/titulo.png", (w-logo_width)/2, h-100, width=logo_width, height=logo_height)
     except:
         pass
 
@@ -898,7 +898,7 @@ def funcionario_print_day():
     usuario = session["username"]
     fecha_imp = datetime.now().strftime("%A %d de %B de %Y a las %I:%M %p").capitalize()
 
-    c.setFont("Helvetica", 9)
+    c.setFont("Helvetica-Bold", 9)
     c.drawString(250, 55, f"IMPRESO POR: {usuario} — {fecha_imp}")
 
     c.save()
